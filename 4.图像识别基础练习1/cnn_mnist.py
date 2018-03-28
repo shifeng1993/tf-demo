dd67453c0611da43d6b4a3eb084528e6bacc11e5
shifeng <shifeng199307@gmail.com>
2018-03-28 22:42:31 +0800

    3.28

--- 4.图像识别基础练习1/cnn_mnist.py
+++ 4.图像识别基础练习1/cnn_mnist.py
@@ -1,41 +1,28 @@
-
-#  Copyright 2016 The TensorFlow Authors. All Rights Reserved.
-#
-#  Licensed under the Apache License, Version 2.0 (the "License");
-#  you may not use this file except in compliance with the License.
-#  You may obtain a copy of the License at
-#
-#   http://www.apache.org/licenses/LICENSE-2.0
-#
-#  Unless required by applicable law or agreed to in writing, software
-#  distributed under the License is distributed on an "AS IS" BASIS,
-#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#  See the License for the specific language governing permissions and
-#  limitations under the License.
-"""Convolutional Neural Network Estimator for MNIST, built with tf.layers."""
-
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
 import numpy as np
 import tensorflow as tf
 
 tf.logging.set_verbosity(tf.logging.INFO)
 
+# batch_size：每次训练使用的训练集的子集的大小
+# image_width：图像的宽
+# image_height：图像的高
+# channels：彩色图像值为3，黑白图像值为1
+
+# https://www.jianshu.com/p/96440832d7b9
+
 
 def cnn_model_fn(features, labels, mode):
-  """Model function for CNN."""
   # 输入层
-  # Reshape X to 4-D tensor: [batch_size, width, height, channels]
-  # MNIST images are 28x28 pixels, and have one color channel
+  # 输入层的tensor（input_layer）一共有4个维度，分别是：[ batch_size, image_width, image_height, channels]
+  # MNIST 图像是28x28像素, 一个颜色通道
+  # features将大小为28×28的黑白照片通过feature参数传递进来（每个值代表一个像素），然后reshape成4-D的输入层tensor。batch_size取-1表示该值是在其他3个值不变的情况下，根据feature的长度动态计算出的。这样做的好处是可以把batch_size作为一个可以调的hyperparameter。
   input_layer = tf.reshape(features["x"], [-1, 28, 28, 1])
 
-  # Convolutional Layer #1
-  # Computes 32 features using a 5x5 filter with ReLU activation.
-  # Padding is added to preserve width and height.
-  # Input Tensor Shape: [batch_size, 28, 28, 1]
-  # Output Tensor Shape: [batch_size, 28, 28, 32]
+  # 卷积层 #1
+  # 计算 32 个特征，使用5*5的卷积核，并使用relu激活函数来输出非线性结果
+  # padding=‘same’：通过zero-padding的办法，保证input和output tensor的大小一致。
+  # Input 张量形状: [batch_size, 28, 28, 1]
+  # Output 张量形状: [batch_size, 28, 28, 32]
   conv1 = tf.layers.conv2d(
       inputs=input_layer,
       filters=32,
@@ -43,17 +30,17 @@ def cnn_model_fn(features, labels, mode):
       padding="same",
       activation=tf.nn.relu)
 
-  # Pooling Layer #1
-  # First max pooling layer with a 2x2 filter and stride of 2
-  # Input Tensor Shape: [batch_size, 28, 28, 32]
-  # Output Tensor Shape: [batch_size, 14, 14, 32]
+  # 池化层 #1
+  # 2*2 最大池化，步长为2
+  # Input 张量形状: [batch_size, 28, 28, 32]
+  # Output 张量形状: [batch_size, 14, 14, 32]
   pool1 = tf.layers.max_pooling2d(inputs=conv1, pool_size=[2, 2], strides=2)
 
-  # Convolutional Layer #2
-  # Computes 64 features using a 5x5 filter.
-  # Padding is added to preserve width and height.
-  # Input Tensor Shape: [batch_size, 14, 14, 32]
-  # Output Tensor Shape: [batch_size, 14, 14, 64]
+  # 卷积层 #2
+  # 计算64个特征，使用 5*5的卷积核，并使用relu激活函数来输出非线性结果
+  # 添加内宽度 Padding 以保证宽高
+  # Input 张量形状: [batch_size, 14, 14, 32]
+  # Output 张量形状: [batch_size, 14, 14, 64]
   conv2 = tf.layers.conv2d(
       inputs=pool1,
       filters=64,
@@ -61,80 +48,79 @@ def cnn_model_fn(features, labels, mode):
       padding="same",
       activation=tf.nn.relu)
 
-  # Pooling Layer #2
-  # Second max pooling layer with a 2x2 filter and stride of 2
-  # Input Tensor Shape: [batch_size, 14, 14, 64]
-  # Output Tensor Shape: [batch_size, 7, 7, 64]
+  # 池化层 #2
+  # 2*2 最大池化，步长为2
+  # Input 张量形状: [batch_size, 14, 14, 64]
+  # Output 张量形状: [batch_size, 7, 7, 64]
   pool2 = tf.layers.max_pooling2d(inputs=conv2, pool_size=[2, 2], strides=2)
 
-  # Flatten tensor into a batch of vectors
-  # Input Tensor Shape: [batch_size, 7, 7, 64]
-  # Output Tensor Shape: [batch_size, 7 * 7 * 64]
+  # 将张量转化为向量
+  # Input 张量形状: [batch_size, 7, 7, 64]
+  # Output 张量形状: [batch_size, 7 * 7 * 64]
   pool2_flat = tf.reshape(pool2, [-1, 7 * 7 * 64])
 
-  # Dense Layer
-  # Densely connected layer with 1024 neurons
-  # Input Tensor Shape: [batch_size, 7 * 7 * 64]
-  # Output Tensor Shape: [batch_size, 1024]
+  # 全连接层 #1
+  # 全连接层有 1024个神经元，使用ReLU激活函数
+  # Input 张量形状: [batch_size, 7 * 7 * 64]
+  # Output 张量形状: [batch_size, 1024]
   dense = tf.layers.dense(inputs=pool2_flat, units=1024, activation=tf.nn.relu)
 
-  # Add dropout operation; 0.6 probability that element will be kept
+  # 添加 dropout 操作; dropout率0.4 (为了避免过拟合，在训练的时候，40%的神经元会被随机去掉）
   dropout = tf.layers.dropout(
       inputs=dense, rate=0.4, training=mode == tf.estimator.ModeKeys.TRAIN)
 
-  # Logits layer
-  # Input Tensor Shape: [batch_size, 1024]
-  # Output Tensor Shape: [batch_size, 10]
+  # 全连接层 #2（Logits Layer）10个神经元，每个神经元对应一个类别（0-9），返回预测的原始值
+  # Input 张量形状: [batch_size, 1024]
+  # Output 张量形状: [batch_size, 10]
   logits = tf.layers.dense(inputs=dropout, units=10)
 
+  # 定义字典 后面调用
   predictions = {
-      # Generate predictions (for PREDICT and EVAL mode)
+      # 生成的预测（预测和评估模式）
       "classes": tf.argmax(input=logits, axis=1),
-      # Add `softmax_tensor` to the graph. It is used for PREDICT and by the
-      # `logging_hook`.
       "probabilities": tf.nn.softmax(logits, name="softmax_tensor")
   }
   if mode == tf.estimator.ModeKeys.PREDICT:
     return tf.estimator.EstimatorSpec(mode=mode, predictions=predictions)
 
-  # Calculate Loss (for both TRAIN and EVAL modes)
+  # 计算损失
   loss = tf.losses.sparse_softmax_cross_entropy(labels=labels, logits=logits)
 
-  # Configure the Training Op (for TRAIN mode)
+  # 配置训练的Op (用于训练模型)
   if mode == tf.estimator.ModeKeys.TRAIN:
     optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.001)
     train_op = optimizer.minimize(
-        loss=loss,
-        global_step=tf.train.get_global_step())
+        loss=loss, global_step=tf.train.get_global_step())
     return tf.estimator.EstimatorSpec(mode=mode, loss=loss, train_op=train_op)
 
-  # Add evaluation metrics (for EVAL mode)
+  # 添加测试标准 (测试模式)
   eval_metric_ops = {
-      "accuracy": tf.metrics.accuracy(
-          labels=labels, predictions=predictions["classes"])}
+      "accuracy":
+      tf.metrics.accuracy(labels=labels, predictions=predictions["classes"])
+  }
   return tf.estimator.EstimatorSpec(
       mode=mode, loss=loss, eval_metric_ops=eval_metric_ops)
 
 
 def main(unused_argv):
-  # Load training and eval data
+  # 加载训练 和 测试数据
   mnist = tf.contrib.learn.datasets.load_dataset("mnist")
   train_data = mnist.train.images  # Returns np.array
   train_labels = np.asarray(mnist.train.labels, dtype=np.int32)
   eval_data = mnist.test.images  # Returns np.array
   eval_labels = np.asarray(mnist.test.labels, dtype=np.int32)
 
-  # Create the Estimator
+  # 创建一个评估测试
   mnist_classifier = tf.estimator.Estimator(
-      model_fn=cnn_model_fn, model_dir="/tmp/mnist_convnet_model")
+      model_fn=cnn_model_fn, model_dir="./mnist_convnet_model")
 
-  # Set up logging for predictions
-  # Log the values in the "Softmax" tensor with label "probabilities"
+  # 建立预测日志
+  # 用 "probabilities" 标签记录 “Softmax” 张量中的值
   tensors_to_log = {"probabilities": "softmax_tensor"}
   logging_hook = tf.train.LoggingTensorHook(
       tensors=tensors_to_log, every_n_iter=50)
 
-  # Train the model
+  # 训练这个模型
   train_input_fn = tf.estimator.inputs.numpy_input_fn(
       x={"x": train_data},
       y=train_labels,
@@ -142,16 +128,11 @@ def main(unused_argv):
       num_epochs=None,
       shuffle=True)
   mnist_classifier.train(
-      input_fn=train_input_fn,
-      steps=1000,
-      hooks=[logging_hook])
+      input_fn=train_input_fn, steps=30000, hooks=[logging_hook])
 
-  # Evaluate the model and print results
+  # 测试和打印结果
   eval_input_fn = tf.estimator.inputs.numpy_input_fn(
-      x={"x": eval_data},
-      y=eval_labels,
-      num_epochs=1,
-      shuffle=False)
+      x={"x": eval_data}, y=eval_labels, num_epochs=1, shuffle=False)
   eval_results = mnist_classifier.evaluate(input_fn=eval_input_fn)
   print(eval_results)
 
