339446403dae04a10db929c46cfb25efe93a8015
shifeng <shifeng199307@gmail.com>
2018-06-09 20:32:13 +0800

    修复指令集报错

--- 1.tf基本用法/1.平面拟合.py
+++ 1.tf基本用法/1.平面拟合.py
@@ -1,5 +1,8 @@
+import os
 import tensorflow as tf
 import numpy as np
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 # 1.准备数据：使用 NumPy 生成假数据(phony data), 总共 100 个点.
 x_data = np.random.rand(2, 100)  # 随机输入
